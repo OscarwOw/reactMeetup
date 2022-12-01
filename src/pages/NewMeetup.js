4c487c2fc1d0f8ec821c52629018aafb07f9c1a8
OscarwOw <OscarwOw210@gmail.com>
2022-12-01 18:12:14 +0100

    http post

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    const navigate = useNavigate();
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-meetup-oscar-default-rtdb.europe-west1.firebasedatabase.app/meetups.json', {
+            method: 'Post',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Contect-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            navigate('/')
+        });
+    }
     return <section>
         <h1>Add New Meetup</h1>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
 }
 
