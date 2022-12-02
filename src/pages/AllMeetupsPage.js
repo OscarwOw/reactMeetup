11f7b74f3641bfe5abd1ebf287bdc728bdd84b6c
OscarwOw <OscarwOw210@gmail.com>
2022-12-02 11:16:49 +0100

    React is pure pain my eyes are crying

--- src/pages/AllMeetupsPage.js
+++ src/pages/AllMeetupsPage.js
@@ -1,4 +1,6 @@
 import MeetupList from '../components/meetups/MeetupList';
+import { useState, useEffect } from 'react';
+
 
 const Dummy_Data = [
     {
@@ -22,9 +24,39 @@ const Dummy_Data = [
 ];
 
 function AllMeetupsPage() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch('https://react-meetup-oscar-default-rtdb.europe-west1.firebasedatabase.app/meetups.json').then(response => {
+            return response.json();
+        }).then(data => {
+            const meetups = [];
+            for (const key in data) {
+                const meetup = {
+                    id: key,
+                    ...data[key]
+                };
+                meetups.push(meetup);
+            }
+
+            setIsLoading(false);
+            setLoadedMeetups(meetups);
+        });
+    }, []);
+
+    
+
+    if (isLoading) {
+        return <section>
+            <p>Loading..</p>
+        </section>
+    }
+
     return <section>
         <h1>All Meetups Page</h1>
-        <MeetupList meetups={Dummy_Data} />
+        <MeetupList meetups={loadedMeetups} />
     </section>;
 }
 
